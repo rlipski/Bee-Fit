d8e129f47f3f4de665b280725eeb02d40b7febd4
Radosław Lipski <rlipski76@gmail.com>
2019-09-30 18:06:00 +0200

    initial commit

---
+++ BeeFit.API/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeeFit.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+    }
+}
